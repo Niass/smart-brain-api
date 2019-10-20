f0c463e613ef9b90dbe1662dca353a419e137a26
Ibrahima Niass <cheroubawan@hotmail.fr>
2019-10-20 22:41:48 +0200

    Did little code review and moved some functions to separate files

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 
 const knex = require("knex");
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 const db = knex({
   client: "pg",
@@ -14,13 +18,6 @@ const db = knex({
   }
 });
 
-// db.select('*').from('users').then(data => {
-//   console.log(data);
-// });
-
-// const saltRounds = 10;
-// const myPlaintextPassword = "s0//P4$$w0rD";
-const someOtherPlaintextPassword = "not_bacon";
 const app = express();
 const cors = require("cors");
 app.use(bodyParser.json());
@@ -57,99 +54,14 @@ app.get("/", (req, res) => {
   res.json(database.users);
 });
 
-app.post("/signin", (req, res) => {
-  db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
-    .then(data => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if(isValid) {
-        return db.select('*').from('users')
-          .where('email', '=', req.body.email)
-          .then(user => {
-            res.json(user[0])
-          })
-          .catch(err => res.status(400).json('unable to get user'))
-      } else {
-        res.status(400).json('wrong credentials')
-      }
-    })
-    .catch(err => res.status(400).json('wrong credentials'))
-});
+app.post("/signin", (req, res) => signin.handleSignin(req, res, db, bcrypt));
 
-app.post("/register", (req, res) => {
-  const { email, name, password } = req.body;
-  const saltRounds = 10;
-  //   bcrypt.genSalt(saltRounds, function(err, salt) {
-  //     bcrypt.hash(password, salt, function(err, hash) {
-  //         // Store hash in your password DB.
-  //         console.log(hash);
-  //     });
-  // });
-  const salt = bcrypt.genSaltSync(saltRounds);
-  const hash = bcrypt.hashSync(password, salt);
-  db.transaction(trx => {
-    trx
-      .insert({
-        hash: hash,
-        email: email
-      })
-      .into("login")
-      .returning("email")
-      .then(loginEmail => {
-        return trx("users")
-          .returning("*")
-          .insert({
-            email: loginEmail[0],
-            name: name,
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0]);
-          })
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-      })
-      .catch(err => res.status(400).json('unable to register'));
-    });
+app.post("/register",(req, res) => register.handleRegister(req, res, db, bcrypt));
 
-app.get("/profile/:id", (req, res) => {
-  const { id } = req.params;
-  db.select("*")
-    .from("users")
-    .where({
-      id
-    })
-    .then(user => {
-      if (user.length) {
-        res.json(user[0]);
-      } else {
-        res.status(400).json("Not found");
-      }
-    })
-    .catch(err => res.status(400).json("error getting user"));
-});
-app.put("/image", (req, res) => {
-  const { id } = req.body;
-  db("users")
-    .where("id", "=", id)
-    .increment("entries", 1)
-    .returning("entries")
-    .then(entries => res.json(entries[0]))
-    .catch(err => res.status(400).json("unable to get entries"));
-});
+app.get("/profile/:id", (req, res) => profile.handleProfile(req, res, db));
+
+app.put("/image", (req, res) => image.handleImage(req, res, db) );
 
 app.listen(3001, () => {
   console.log("app is runnig on port 3000");
 });
-
-/**
- *
- * /--> res = this is working
- * /signin --> POST = success/fail
- * /register -- > POST = user
- * /profile/:userId --> GET = user
- * /image --> PUT --> user
- *
- *
- */
